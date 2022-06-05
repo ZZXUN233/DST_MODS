17277cd29047815f0c0d32ca9ac19b53d494ee6e
zzxunoo <zhangzhaoxun@outlook.com>
2022-06-06 00:12:16 +0800

    快捷删除调整

--- mod-del-tools/modmain.lua
+++ mod-del-tools/modmain.lua
@@ -1,10 +1,12 @@
 -- 1. 初始化删除
 -- 永久删除配置
-local del_mode = GetModConfigData("del_mode")
 -- 删除撤销缓存
 local delStack = {}
 -- delete按键按住标志
 local deleteKeyDown = false
+
+-- shift按住标志
+local shiftKeyDown = false
 
 -- 判断是否属于装填物品
 local function InInv(b)
@@ -28,31 +30,23 @@ local function CanDelete(inst)
 end
 
 -- 按模式删除
-local function delInMode(obj, isRight)
+local function delInMode(obj)
     -- 不能删除
     if not CanDelete(obj) then return end
-    -- 默认删除模式
-    if del_mode == 0 then
+    if shiftKeyDown then
+        -- 永久删除模式
+        obj:Remove()
+    else
+        -- 隐藏删除
         table.insert(delStack, obj)
         obj:RemoveFromScene()
-        -- 永久删除模式
-    elseif del_mode == 1 then
-        obj:Remove()
-        -- 智能删除模式
-    else
-        -- 右键永久删除
-        if isRight then
-            obj:Remove()
-        else
-            table.insert(delStack, obj)
-            obj:RemoveFromScene()
-        end
     end
+
 
 end
 
 -- 范围删除
-local function delRange(x, y, forverFlag)
+local function delRange(x, y)
     local ents = GLOBAL.TheSim:FindEntities(x, y, 30, 3)
     for _, obj in pairs(ents) do
         if CanDelete(obj) then
@@ -75,7 +69,7 @@ local function delRange(x, y, forverFlag)
                 "lighterfire" or "torchfire" or "torchfire_rag" or
                 "torchfire_spooky" or "torchfire_shadow")) or
                 (obj.entity:GetParent() == nil) then
-                delInMode(obj, forverFlag)
+                delInMode(obj)
             end
         end
     end
@@ -86,14 +80,22 @@ end
 print(
     "+++++++++++++++++++++++++++++++快捷删除加载+++++++++++++++++++++++++++++++++")
 GLOBAL.TheInput:AddKeyHandler(function(key, down) -- 监听键盘事件
-    print("点击事件：key = ", key)
-    deleteKeyDown = down
+    print("点击事件：key = ", key, "down=", down)
+
+    if key == GLOBAL.KEY_SHIFT or key == GLOBAL.KEY_RSHIFT or key == GLOBAL.KEY_LSHIFT then
+        shiftKeyDown = down
+    end
+
     if key == GLOBAL.KEY_DELETE then -- [
         -- todo: 触发删除
-        local obj = GLOBAL.TheInput:GetWorldEntityUnderMouse()
-        delInMode(obj, false)
+        deleteKeyDown = down -- 删除键状态
+        if down then
+            local obj = GLOBAL.TheInput:GetWorldEntityUnderMouse()
+            delInMode(obj)
+        end
     end
 end)
+
 
 -- MOUSEBUTTON_LEFT = 1000
 -- MOUSEBUTTON_RIGHT = 1001
@@ -103,21 +105,15 @@ GLOBAL.TheInput:AddMouseButtonHandler(function(button, down, x, y)
     -- 鼠标左键
     if button == GLOBAL.MOUSEBUTTON_LEFT and down then
         if deleteKeyDown then
-            delRange(x, y, false)
+            delRange(x, y)
         end
-    -- 鼠标右键
+        -- 鼠标右键
     elseif button == GLOBAL.MOUSEBUTTON_RIGHT and down then
-        if deleteKeyDown then
-            print("永久删除！")
-            delRange(x, y, true)
-        else
-            print("撤销删除！")
-            local reObj = table.remove(delStack)
-            if reObj ~= nil then reObj:ReturnToScene() end
-        end
+        print("撤销删除！")
+        local reObj = table.remove(delStack)
+        if reObj ~= nil then reObj:ReturnToScene() end
     end
 end)
 -- 3.1 获取鼠标点击的物品
 print(
     "++++++++++++++++++++++++++++++快捷删除加载完成++++++++++++++++++++++++++++++++")
-
